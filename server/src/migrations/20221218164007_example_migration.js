e192d08b0ff906ca35a888676e250139e3db7085
GritzMaze <jivkovvesselin@gmail.com>
2022-12-18 18:48:26 +0200

    Add a example migration

---
+++ server/src/migrations/20221218164007_example_migration.js
@@ -0,0 +1,33 @@
+/**
+ * THIS IS AN EXAMPLE MIGRATION
+ */
+
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  /**
+    await knex.schema.createTable('example_table', (table) => {
+    table.string('name');
+    table.boolean('is_active');
+    table.integer('age');
+        
+    // required fields for every model
+    table.increments('id').primary();
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+  });
+   */
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  /**
+  await knex.schema.dropTable('example_table');
+   */
+};
