3b64d660296b9ce87cc9e5d865fdbe45ec725fc6
Boyan Donchev <boyan.donchev@wolfpack-dcs.com>
2023-02-18 18:14:28 +0200

    use auth header

--- server/src/middlewares/isLoggedIn.js
+++ server/src/middlewares/isLoggedIn.js
@@ -1,14 +1,19 @@
 import jwt from 'jsonwebtoken';
 
 export default function isLoggedIn(req, res, next) {
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
+  const token = req.headers.authorization;
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
   try {
-    const decoded = jwt.verify(token, process.env.SECRET);
+    const [tokenType, jwtToken] = token.split(' ');
+    if (tokenType !== 'Bearer') {
+      throw new Error();
+      return;
+    }
+
+    const decoded = jwt.verify(jwtToken, process.env.SECRET);
     req.user = decoded;
   } catch (err) {
     return res.status(401).send("Invalid Token");
