05ccd6ac1e8c17082feabd4c0b65e88a0e673b33
GritzMaze <jivkovvesselin@gmail.com>
2023-02-19 01:05:06 +0200

    fix wrong binds of components

--- client/src/services/socket.service.js
+++ client/src/services/socket.service.js
@@ -1,6 +1,7 @@
 import io from 'socket.io-client';
 
 import { gameScreenComponent } from '../components/game-screen/game-screen.js'
+import { tokenService } from './token.service.js';
 
 class SocketService {
   constructor() {
@@ -9,43 +10,50 @@ class SocketService {
     this.clientRoom;
     this.gameOn = false;
     this.handleSockets();
+    this.components = [];
   }
 
   emitMessage = (event, data) => {
     this.socket.emit(event, data);
   }
 
+  appendComponent = (component) => {
+    this.components.push(component);
+  }
+
   handleSockets = () => {
     this.socket.on('serverMsg', data => {
       this.clientRoom = data.roomNo;
       this.userId = data.socketId;
+      this.user = tokenService.getDecodedToken();
+      this.emitMessage('currentUser', {userId: this.user.userId, roomNo: this.clientRoom});
     });
 
     this.socket.on('receiveChatMessage', response => {
       const isOur = response.userId === this.userId;
-      gameScreenComponent.recieveMesageFromSocket(response, isOur);
+      this.components.map(comp => comp.recieveMesageFromSocket(response, isOur));
     });
 
     this.socket.on('receiveGameMessage', response => {
       if (response.message.playOn === true && response.playersReady == 2) {
         this.gameOn = true;
-        gameScreenComponent.startGame();
+        this.components.map(comp => comp.startGame());
       } else if (response.message.playOn === true) {
         const isOur = response.userId === this.userId;
-        gameScreenComponent.hideBoard(isOur);
+        this.components.map(comp => comp.hideBoard(isOur));
       } else if (response.message.playOn === false) {
         this.gameOn = false;
-        gameScreenComponent.endGame();
+        this.components.map(comp => comp.endGame());
       }
 
       if (response.message.movedPart) {
         const isOur = response.userId === this.userId;
-        gameScreenComponent.emitBoard(isOur);
+        this.components.map(comp => comp.emitBoard(isOur));
       }
 
       if (response.message.innerHtml) {
         const isOur = response.userId === this.userId;
-        gameScreenComponent.updatePlayerB(response.message.innerHtml, isOur);
+        this.components.map(comp => comp.updatePlayerB(response.message.innerHtml, isOur));
       }
     });
   }
@@ -55,7 +63,7 @@ class SocketService {
       if (response.message.isWin !== undefined) {
         const isOur = response.userId === this.userId;
         if (!isOur) {
-          triggerFunc(response.message.isWin, isOur);
+          triggerFunc(response.message.isWin, isOur, response.players);
           socketService.emitMessage('gameMessage', {playOn: false});
         }
       }
